b04f51d06d76c8b042d39d45c42b11198e4a0136
Thalita Alcantara <thalita.alcantara7@gmail.com>
2024-06-27 20:30:14 +0100

    repetição

---
+++ meu primeiro projeto java/src/ExemploFor.java
@@ -0,0 +1,16 @@
+public class ExemploFor {
+ /* public static void main(String[] args) {
+    for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+      System.out.println("Contando carneirinhos " + carneirinhos);
+    }
+
+    System.out.println("João dormiu");
+  }*/
+  public static void main(String[] args) {
+    String alunos [] = {"Felipe", "Jonas","Julia","Marcus"};
+    for(int x=0; x < alunos.length; x++){
+      System.out.println("O aluno no indice x = " + x + " é " + alunos [x]);
+    }
+  }
+
+}
